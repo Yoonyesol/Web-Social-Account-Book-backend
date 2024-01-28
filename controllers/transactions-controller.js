b7441d37fcc804b83e8d25d72cdd8d777ae6d443
Yoonyesol <maniac970430@naver.com>
2024-01-28 17:39:32 +0900

    ✨feat: [BE+DB] 데이터베이스에서 transaction 문서 불러오기, 문서 수정/삭제하기 (GET, PATCH, DELETE)

--- controllers/transactions-controller.js
+++ controllers/transactions-controller.js
@@ -39,33 +39,50 @@ let DUMMY_TRANSACTION = [
   },
 ];
 
-const getTransactionById = (req, res, next) => {
+const getTransactionById = async (req, res, next) => {
   const transactionId = req.params.tid; // type: string
 
-  const transaction = DUMMY_TRANSACTION.find((t) => {
-    return t.tid === transactionId;
-  });
-
-  // 에러 핸들링
-  if (!transaction) {
-    throw new HttpError("해당 ID에 대한 입출금내역을 찾지 못했습니다.", 404);
+  let transaction;
+  try {
+    transaction = await Transaction.findById(transactionId);
+  } catch (e) {
+    //GET 요청에 문제가 생겼을 때
+    const error = new HttpError("입출금내역을 불러오지 못했습니다.", 500);
+    return next(error);
   }
 
-  res.json({ transaction: transaction });
+  // GET 요청에 문제가 없지만 입출금내역을 찾을 수 없는 경우 에러 핸들링
+  if (!transaction) {
+    const error = new HttpError(
+      "해당 ID에 대한 입출금내역을 찾지 못했습니다.",
+      404
+    );
+    return next(error);
+  }
+
+  res.json({ transaction: transaction.toObject({ getters: true }) });
 };
 
-const getTransactionsByUserId = (req, res, next) => {
+const getTransactionsByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  const transactions = DUMMY_TRANSACTION.filter((u) => {
-    return u.uid === userId;
-  });
-
-  if (!transactions || transactions.length === 0) {
-    throw new HttpError("해당 유저의 입출금내역을 찾지 못했습니다.", 404);
+  let transactions;
+  try {
+    transactions = await Transaction.find({ uid: userId });
+  } catch (e) {
+    const error = new HttpError("입출금내역을 불러오지 못했습니다.", 500);
+    return next(error);
   }
 
-  res.json({ transactions });
+  if (!transactions || transactions.length === 0) {
+    return next(
+      new HttpError("해당 유저의 입출금내역을 찾지 못했습니다.", 404)
+    );
+  }
+
+  res.json({
+    transactions: transactions.map((t) => t.toObject({ getters: true })),
+  });
 };
 
 const createTransaction = async (req, res, next) => {
@@ -88,37 +105,71 @@ const createTransaction = async (req, res, next) => {
   try {
     await createdTransaction.save();
   } catch (e) {
-    const error = new HttpError("입출금 내역 저장 실패", 500);
+    const error = new HttpError(
+      "해당 ID의 입출금내역을 불러오지 못했습니다.",
+      500
+    );
     return next(error);
   }
 
   res.status(201).json({ transaction: createdTransaction });
 };
 
-const updateTransaction = (req, res, next) => {
-  const { category, title, amount, transaction_type, memo } = req.body;
+const updateTransaction = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(HttpError(errors.array(), 422));
+  }
+
+  const { date, category, title, amount, memo } = req.body;
   const transactionId = req.params.tid;
 
-  const updatedTransaction = {
-    ...DUMMY_TRANSACTION.find((t) => t.tid === transactionId),
-  };
-  const transactionIndex = DUMMY_TRANSACTION.findIndex(
-    (t) => t.tid === transactionId
-  );
-  updatedTransaction.category = category;
-  updatedTransaction.title = title;
-  updatedTransaction.amount = amount;
-  updatedTransaction.transaction_type = transaction_type;
-  updatedTransaction.memo = memo;
+  //id로 해당 입출금 내역 불러오기
+  let transaction;
+  try {
+    transaction = await Transaction.findById(transactionId);
+  } catch (e) {
+    const error = new HttpError("입출금내역을 불러오지 못했습니다.", 500);
+    return next(error);
+  }
 
-  DUMMY_TRANSACTION[transactionIndex] = updatedTransaction;
+  //내용 업데이트
+  transaction.date = date;
+  transaction.category = category;
+  transaction.title = title;
+  transaction.amount = amount;
+  transaction.memo = memo;
 
-  res.status(200).json({ transaction: updatedTransaction });
+  try {
+    await transaction.save();
+  } catch (e) {
+    const error = new HttpError("입출금 내역 수정 실패", 500);
+    return next(error);
+  }
+
+  res
+    .status(200)
+    .json({ transaction: transaction.toObject({ getters: true }) });
 };
 
-const deleteTransaction = (req, res, next) => {
+const deleteTransaction = async (req, res, next) => {
   const transactionId = req.params.tid;
   DUMMY_TRANSACTION = DUMMY_TRANSACTION.filter((t) => t.tid !== transactionId);
+
+  let transaction;
+  try {
+    transaction = await Transaction.findById(transactionId);
+  } catch (e) {
+    const error = new HttpError("입출금내역을 불러오지 못했습니다.", 500);
+    return next(error);
+  }
+
+  try {
+    await transaction.deleteOne({ id: transactionId });
+  } catch (e) {
+    const error = new HttpError("입출금내역을 삭제하지 못했습니다.", 500);
+    return next(error);
+  }
 
   res.status(200).json({ message: "삭제 완료", transactionId });
 };
