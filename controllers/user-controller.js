bcf82bb2a99cca4934444e44998dc434d66811f9
Yoonyesol <maniac970430@naver.com>
2024-02-06 21:49:47 +0900

    ✨feat: 리덕스를 이용한 사용자 데이터 전역 관리 (로그인 상태 저장, 사용자 데이터 저장 및 불러오기)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -88,7 +88,16 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: "로그인 성공!" });
+  const userInfo = new User({
+    id: existingUser.id,
+    email: existingUser.email,
+    name: existingUser.username,
+    image: existingUser.image,
+    transactions: existingUser.transactions,
+    communityPosts: existingUser.communityPosts,
+  });
+
+  res.json({ userInfo: userInfo.toObject({ getters: true }) });
 };
 
 exports.getUsers = getUsers;
